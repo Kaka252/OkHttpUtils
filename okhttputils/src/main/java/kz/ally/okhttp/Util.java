edb0c3757d8cb26f348f02d5d47eeb310fc645e2
zhouyou <kaka252@sina.com>
2017-03-13 15:55:24 +0800

    添加下载文件的进度控制

--- okhttputils/src/main/java/kz/ally/okhttp/Util.java
+++ okhttputils/src/main/java/kz/ally/okhttp/Util.java
@@ -7,19 +7,21 @@ package kz.ally.okhttp;
 public class Util {
 
     public static String castString(Object o) {
-        String value = "";
-        if (o instanceof Long) {
+        String value;
+        if (o instanceof String) {
+            value = (String) o;
+        } else if (o instanceof Integer) {
+            value = String.valueOf(o);
+        } else if (o instanceof Boolean) {
+            value = String.valueOf(o);
+        } else if (o instanceof Long) {
             value = String.valueOf(o);
         } else if (o instanceof Float) {
             value = String.valueOf(o);
         } else if (o instanceof Short) {
             value = String.valueOf(o);
-        } else if (o instanceof Integer) {
-            value = String.valueOf(o);
-        } else if (o instanceof Boolean) {
-            value = String.valueOf(o);
-        } else if (o instanceof String) {
-            value = (String) o;
+        } else {
+            value = o.toString();
         }
         return value;
     }
