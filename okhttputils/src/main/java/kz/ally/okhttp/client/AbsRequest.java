4e29e85ced4a80ef388026ca1705ebea625419df
zhouyou <kaka252@sina.com>
2017-05-22 20:47:33 +0800

    优化

--- okhttputils/src/main/java/kz/ally/okhttp/client/AbsRequest.java
+++ okhttputils/src/main/java/kz/ally/okhttp/client/AbsRequest.java
@@ -2,11 +2,15 @@ package kz.ally.okhttp.client;
 
 import com.google.gson.annotations.Expose;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 
+import kz.ally.okhttp.callback.MainThread;
 import kz.ally.okhttp.callback.ObjCallback;
 import kz.ally.okhttp.config.Params;
-import kz.ally.okhttp.response.ApiResponse;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 /**
  * Author: ZhouYou
@@ -22,8 +26,33 @@ public abstract class AbsRequest<T extends AbsResponse> {
 
     private ObjCallback<T> mCallback;
 
-    public ApiResponse getRawResponseCallback() {
-        return new ApiResponse(mCallback);
+    public Callback getRawResponseCallback() {
+        return new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                errorCallback(mCallback, call, e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (mCallback != null) {
+                    try {
+                        if (response.isSuccessful()) {
+                            T result = mCallback.parseResponse(response);
+                            parseCallback(mCallback, result);
+                        } else {
+                            errorCallback(mCallback, call, null);
+                        }
+                    } catch (Exception e) {
+                        errorCallback(mCallback, call, e);
+                    } finally {
+                        if (response.body() != null) {
+                            response.body().close();
+                        }
+                    }
+                }
+            }
+        };
     }
 
     public abstract String getUrl();
@@ -45,4 +74,38 @@ public abstract class AbsRequest<T extends AbsResponse> {
         return params;
     }
 
+    /**
+     * 请求失败后回调
+     *
+     * @param callback
+     * @param call
+     * @param e
+     */
+    private void errorCallback(final ObjCallback callback, final Call call, final Exception e) {
+        MainThread.getInstance().execute(new Runnable() {
+            @Override
+            public void run() {
+                if (call.isCanceled()) {
+                    callback.onError(call, null);
+                } else {
+                    callback.onError(call, e);
+                }
+            }
+        });
+    }
+
+    /**
+     * 请求成功后回调
+     *
+     * @param callback
+     * @param result
+     */
+    private void parseCallback(final ObjCallback callback, final T result) {
+        MainThread.getInstance().execute(new Runnable() {
+            @Override
+            public void run() {
+                callback.onResponse(result);
+            }
+        });
+    }
 }
